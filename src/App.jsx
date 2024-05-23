5db645cf089d752e82071f42c1481368ba73f1c9
Keane <keane@usernoMacBook-Pro.local>
2024-05-23 16:18:26 +0900

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import "./App.css";
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
